5a65e955f124bf86d3fdee447b7fdf3b15cd8cb7
Billy Brylle Pablo Mendoza <billybryllepablo.mendoza@outlook.com>
2019-08-27 08:27:55 -0700

    Initial

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var Name = "";
+            if(Id == 1)
+            {
+                Name = "John Smith";
+            }else if(Id == 2)
+            {
+                Name = "Mary Williams";
+            }
+            
+            var customers = new List<Customer>
+            {
+                new Customer { Name = Name }
+            };
+
+            var viewModel = new ViewModels.RandomMovieViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
